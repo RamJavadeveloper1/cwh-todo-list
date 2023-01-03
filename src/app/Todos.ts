f4f68c31e5fd37ab9d3cc6875639363100153836
Ramkishorgautam1 <ramkishorgautam1@gmail.com>
2023-01-03 18:25:59 +0530

    second commit

---
+++ src/app/Todos.ts
@@ -0,0 +1,7 @@
+export class Todos{
+sno!:number
+title!:string
+desc!:string
+active!:boolean
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './myComponates/todo/todo.component';
+import { AddTodoComponent } from './myComponates/add-todo/add-todo.component';
+import { TodoItemComponent } from './myComponates/todo-item/todo-item.component';
+import { AboutTodoComponent } from './myComponates/about-todo/about-todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    AddTodoComponent,
+    TodoItemComponent,
+    AboutTodoComponent
   ],
   imports: [
     BrowserModule,
